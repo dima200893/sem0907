cc0910b2312fa7754aee07f8ee7f6c8e5866c8ea
Dmitry Svyatov <dimka201993@gmail.com>
2022-07-10 11:28:03 +0300

    Задача с нахождение количества четных чисел

---
+++ lesson34/Program.cs
@@ -0,0 +1,52 @@
+﻿int size= InputInt("Введите размер ");
+
+int[]numbers=new int[size];
+
+int count=0;
+
+FillArray(numbers);
+PrintArray(numbers);
+
+for(int i=0;i < numbers.Length; i++)
+{
+    if(numbers[i]%2==0)
+    {
+        count=count+1;
+    }
+}
+
+Console.WriteLine($"В данном массиве количетсво четных чисел = " + (count));
+
+
+
+
+
+
+
+
+
+
+
+int InputInt(string output)
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray (int[] array)
+{
+    for(int i=0;i< array.Length; i++)
+    {
+        array[i]= new Random().Next(99,1000);
+    }
+}
+
+
+void PrintArray (int[] array)
+{
+    for(int i=0;i< array.Length; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+    Console.WriteLine();
+}
